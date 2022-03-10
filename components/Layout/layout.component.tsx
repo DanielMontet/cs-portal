426070df8d5f7f6017cc78da2b2093aa82b27da5
Daniel Montet <daniel@kune.africa>
2022-03-10 13:12:48 +0300

    Added a layout component base

---
+++ components/Layout/layout.component.tsx
@@ -0,0 +1,8 @@
+export default function Layout() {
+  return (
+    <section className="h-screen grid grid-cols-12">
+      <section className="col-span-2 bg-green-200"></section>
+      <section className="col-span-10 bg-orange-200"></section>
+    </section>
+  );
+}
