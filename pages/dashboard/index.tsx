d6cf5af73ebb31af8d54cf7b2a08c813686e2507
Daniel Montet <daniel@kune.africa>
2022-03-11 08:31:05 +0300

    Added dashboard page base

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,7 @@
+import Layout from "../../components/Layout/layout.component";
+
+const Dashboard: React.FunctionComponent<any> = (props) => {
+  return <Layout></Layout>;
+};
+
+export default Dashboard;
