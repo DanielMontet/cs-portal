40a864ba8134b0c1e466a7b38fe25d918fc4970a
Daniel Montet <daniel@kune.africa>
2022-03-14 16:54:39 +0300

    Added routes on the layout navigation

---
+++ src/components/Dashboard/dashboard.component.tsx
@@ -0,0 +1,106 @@
+import { meals } from "../../lib/dummy-data";
+import { AppProps } from "../../types/props.types";
+
+const Dashboard: React.FunctionComponent<AppProps> = (props) => {
+  return (
+    <section className="grid grid-rows-48 h-full">
+      <header className="row-span-4">
+        <div className="flex flex-row items-center px-5 py-4 bg-white rounded-lg shadow">
+          <div className="flex flex-row items-center">
+            <label htmlFor="kuneHubs" className="mr-2">
+              Filter by hub:
+            </label>
+            <select
+              className="border border-slate-500 outline-0 focus:border-blue-700 text-slate-500 text-sm rounded-md p-2 bg-white"
+              name="hubs"
+              id="kuneHubs"
+            >
+              <option className="rounded-md" value="All" selected>
+                ALL HUBS
+              </option>
+              <option value="MAUA">MAUA</option>
+              <option value="CBD">CBD</option>
+              <option value="SAMEER">SAMEER</option>
+              <option value="THIKA ROAD">THIKA ROAD</option>
+            </select>
+          </div>
+        </div>
+      </header>
+      <section className="row-span-42 flex flex-col gap-5 shadow">
+        <div className="grid grid-cols-4 gap-5 mb-3">
+          <div className="bg-primary text-white text-1xl fs rounded-lg h-32 p-5">
+            Total Orders
+          </div>
+          <div className="bg-secondary text-white rounded-lg h-32 p-5 shadow">
+            Another report
+          </div>
+          <div className="bg-warning text-white rounded-lg h-32 p-5 shadow">
+            Another report
+          </div>
+          <div className="bg-dark text-white rounded-lg h-32 p-5 shadow">
+            Another report
+          </div>
+        </div>
+        <div className="bg-white p-5 rounded-lg flex flex-col gap-4">
+          <h2 className="mb-3">Meals of the day</h2>
+          <div className="shadow overflow-x-scroll border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-100">
+                <tr>
+                  <th
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider"
+                    scope="col"
+                  >
+                    Title
+                  </th>
+                  <th
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider"
+                    scope="col"
+                  >
+                    Sold
+                  </th>
+                  <th
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider"
+                    scope="col"
+                  >
+                    Remaining
+                  </th>
+                  <th
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider"
+                    scope="col"
+                  ></th>
+                </tr>
+              </thead>
+              <tbody className=" divide-y divide-gray-200">
+                {meals.map((meal) => {
+                  return (
+                    <tr className="">
+                      <td className="flex items-center gap-3 my-3 pl-5">
+                        <img
+                          src={meal.img}
+                          className="h-12 rounded-lg "
+                          alt=""
+                        />
+                        <p className="whitespace-nowrap text-sm text-gray-700">
+                          {meal.title}
+                        </p>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {meal.sold}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {meal.remaining}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Dashboard;
--- src/components/Layout/layout.component.tsx
+++ src/components/Layout/layout.component.tsx
@@ -1,7 +1,9 @@
 import { AppProps } from "../../types/props.types";
 import PerfectScrollbar from "react-perfect-scrollbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Dashboard from "../Dashboard/dashboard.component";
 
-const Layout: React.FunctionComponent<AppProps> = ({ page }) => {
+const Layout: React.FunctionComponent<AppProps> = () => {
   return (
     <section className="h-screen grid grid-rows-48">
       <header className="row-span-4 grid grid-cols-48">
@@ -11,71 +13,84 @@ const Layout: React.FunctionComponent<AppProps> = ({ page }) => {
         </div>
       </header>
       <section className="row-span-44 grid grid-cols-48">
-        <nav className="col-span-7">
-          <div className="flex flex-col ml-6 mr-4">
-            <label className="my-2 text-current font-medium">Dashboard</label>
-            <a
-              href="/dashboard"
-              className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
-            >
-              Dashboard
-            </a>
-            <a
-              href="/todays-orders"
-              className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
-            >
-              Today's Orders
-            </a>
-            <a
-              href="/dashboard"
-              className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
-            >
-              Customer List
-            </a>
-            <a
-              href="/dashboard"
-              className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
-            >
-              Customer Detail
-            </a>
-            <a
-              href="/dashboard"
-              className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
-            >
-              Users
-            </a>
-          </div>
-          <div className="flex flex-col ml-6 mr-4">
-            <label className="my-2 text-current font-medium">
-              Authentication
-            </label>
-            <a
-              href="/login"
-              className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
-            >
-              Login
-            </a>
-            <a
-              href="/register"
-              className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
-            >
-              Register
-            </a>
-            <a
-              href="/reset-password"
-              className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
-            >
-              Reset Password
-            </a>
-          </div>
-        </nav>
-        <main className="col-span-41  bg-blue-100 rounded-tl-lg pt-6 px-6 grid grid-rows-48">
-          <PerfectScrollbar className="row-span-42">{page}</PerfectScrollbar>
-          <div className="row-span-5"></div>
-        </main>
+        <Router>
+          <nav className="col-span-7">
+            <div className="flex flex-col ml-6 mr-4">
+              <label className="my-2 text-current font-medium">Dashboard</label>
+              <Link
+                to="/dashboard"
+                className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
+              >
+                Dashboard
+              </Link>
+              <Link
+                to="/todays-orders"
+                className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
+              >
+                Today's Orders
+              </Link>
+              <Link
+                to="/customer-list"
+                className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
+              >
+                Customer List
+              </Link>
+              <Link
+                to="/customer-detail"
+                className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
+              >
+                Customer Detail
+              </Link>
+              <Link
+                to="/users"
+                className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
+              >
+                Users
+              </Link>
+            </div>
+            <div className="flex flex-col ml-6 mr-4">
+              <label className="my-2 text-current font-medium">
+                Authentication
+              </label>
+              <Link
+                to="/login"
+                className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
+              >
+                Login
+              </Link>
+              <Link
+                to="/reset-password"
+                className="bg-blue-100 text-slate-600 my-1 rounded-md px-5 py-2"
+              >
+                Reset Password
+              </Link>
+            </div>
+          </nav>
+          <main className="col-span-41  bg-blue-100 rounded-tl-lg pt-6 px-6 grid grid-rows-48">
+            <PerfectScrollbar className="row-span-42">
+              <Switch>
+                <Route path="/dashboard">
+                  <Dashboard />
+                </Route>
+                <Route path="/About">
+                  <Pager />
+                </Route>
+              </Switch>
+            </PerfectScrollbar>
+            <div className="row-span-5"></div>
+          </main>
+        </Router>
       </section>
     </section>
   );
 };
+
+function Pager() {
+  return (
+    <>
+      <p>Hello world</p>
+    </>
+  );
+}
 
 export default Layout;
